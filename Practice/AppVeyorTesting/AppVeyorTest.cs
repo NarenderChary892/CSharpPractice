06090308d50a359e84e1fa794a977b760a90dfbf
NarenderUppala <nuppala@configit.com>
2018-10-10 16:36:24 +0530

    appveyor Test

---
+++ Practice/AppVeyorTesting/AppVeyorTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.AppVeyorTesting {
+  class AppVeyorTest {
+    public void TestAppVeyor() {
+      Console.WriteLine("This is a Sample AppVeyor Checking...");
+    }
+  }
+}
