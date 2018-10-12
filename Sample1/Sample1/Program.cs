ce1e080815be04d0b44b2e16cd356192c08745de
NarenderUppala <nuppala@configit.com>
2018-10-12 11:03:13 +0530

    Sample 1 Project

---
+++ Sample1/Sample1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1 {
+  class Program {
+    static void Main( string[] args ) {
+      Console.WriteLine("This is Sample1 Project...");
+      Console.ReadLine();
+    }
+  }
+}
